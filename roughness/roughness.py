4cf1e942917664136b2b1082de6647ec734b9dba
Christian Tai Udovicic <cj.taiudovicic@gmail.com>
2021-01-12 14:05:17 -0700

    Re-format with black, add tests

--- roughness/roughness.py
+++ roughness/roughness.py
@@ -1,8 +1,9 @@
 """Main module."""
 import numpy as np
-from numpy import sin, cos, tan, arccos, exp, pi
+from numpy import sin, cos, tan, exp, pi
 
-def visible_slopes(theta, azimuth, slope, slope_type='rms', nslopes=90):
+
+def visible_slopes(theta, azimuth, slope, dist="rms", ntheta=90, naz=360):
     """
     Return probability of observering slopes from 0-90 degrees given a rough
     surface (slope, slope_type), corrected for viewing geometery (theta, phi).
@@ -13,73 +14,70 @@ def visible_slopes(theta, azimuth, slope, slope_type='rms', nslopes=90):
     azimuth (num): To-spacecraft azimuth angle from North [deg].
     slope (num): Mean slope of distribution [deg].
     slope_type (str): Slope distribution (rms or tbar).
-    nslopes (num): Number of slopes to sample in 0-90 [deg].
+    ntheta (num): Number of surface slope thetas to sample range [0-90) deg.
+    naz (num): Number of surface slope azimuths to sample range [0-360) deg.
 
     Returns
     -------
     P(slope) (arr, len=nslopes): Probability of viewing each slope in [0-90]
     """
     # Grid of all possible surface vectors (theta 0-90, az 0-360)
-    res = nslopes/90
-    theta_arr = np.radians(np.arange(0, 90, res))
-    azimuth_arr = np.radians(np.arange(0, 360, res))
+    theta_arr = np.radians(np.arange(0, 90, 90 / ntheta))
+    azimuth_arr = np.radians(np.arange(0, 360, 360 / naz))
     theta_surf, azimuth_surf = np.meshgrid(theta_arr, azimuth_arr)
-    
+    cartesian_surf = sph2cart(theta_surf, azimuth_surf)
+
     # Spacecraft unit vector
-    theta_sc = np.radians(theta) 
-    azimuth_sc = np.radians(azimuth)
+    theta, azimuth = (np.radians(theta), np.radians(azimuth))
+    cartesian_sc = sph2cart(theta, azimuth)
 
     # Cartesian dot product gives cos(angle) between vectors
-    cartesian_sc = sph2cart(theta_sc, azimuth_sc)
-    cartesian_surf = sph2cart(theta_surf, azimuth_surf)
-    dot = np.dot(cartesian_surf, cartesian_sc) 
-    # dot[dot > 1] = 1
+    cos_surf_sc = np.dot(cartesian_surf, cartesian_sc)
 
-    # Angle > 90 deg means surface is oriented away from spacecraft
-    # Theses surfaces are not visible, so set to nan
-    angle = arccos(dot)
-    angle[angle >= (np.pi/2)] = np.nan
+    # Negative cos(angle) means surface oriented away from sc, set to 0
+    cos_surf_sc[cos_surf_sc < 0] = 0
+    # cos_surf_sc[cos_surf_sc > 1] = 1  # shouldn't be possible
 
     # Get raw slope distribution and correct for viewing geometry
-    slopes = slope_dist(theta_surf, slope, slope_type)
-    vis_slopes = slopes * cos(angle) / cos(theta_sc)
+    slopes = slope_dist(theta_surf, slope, dist)
+    vis_slopes = slopes * cos_surf_sc / cos(theta)
     # vis_slopes[vis_slopes < 0] = 0
 
     return vis_slopes
 
 
-def sph2cart(theta, phi, r=1):
+def sph2cart(theta, phi, radius=1):
     """
     Convert from spherical (theta, phi, r) to cartesian (x, y, z) coordinates.
 
-    Theta and phi must be specified in radians and returned units correspond to 
-    units of r, default is unitless (r=1 is unit vector). Returns vectors along 
-    z-axis (e.g., if theta and phi are int/float return 1x1x3 vector; if theta 
+    Theta and phi must be specified in radians and returned units correspond to
+    units of r, default is unitless (r=1 is unit vector). Returns vectors along
+    z-axis (e.g., if theta and phi are int/float return 1x1x3 vector; if theta
     and phi are MxN arrays return 3D array of vectors MxNx3).
 
     Parameters
     ----------
     theta (num or arr): Polar angle [rad].
     phi (num or arr): Azimuthal angle [rad].
-    r (num or arr): Radius (default=1).
+    radius (num or arr): Radius (default=1).
 
     Returns
     -------
     MxNx3 arr: Vectors converted to cartesian coordinates.
     """
-    return np.dstack([
-        r * sin(theta) * cos(phi),  # x
-        r * sin(theta) * sin(phi),  # y
-        r * cos(theta)  # z
-    ]).squeeze()
+    return np.dstack(
+        [
+            radius * sin(theta) * cos(phi),
+            radius * sin(theta) * sin(phi),
+            radius * cos(theta),
+        ]
+    ).squeeze()
 
 
-
-
-def slope_dist(theta, theta0, dist='rms'):
+def slope_dist(theta, theta0, dist="rms"):
     """
     Return slope probability distribution for rough fractal surfaces. Computes
-    distributions parameterized by mean slope (theta0) using Shepard 1995 (rms) 
+    distributions parameterized by mean slope (theta0) using Shepard 1995 (rms)
     or Hapke 1984 (tbar) roughness.
 
     Parameters
@@ -93,17 +91,16 @@ def slope_dist(theta, theta0, dist='rms'):
     slopes (arr): Probability of a slope occurring at each theta.
     """
     # Calculate Gaussian RMS slope distribution (Shepard 1995)
-    if dist == 'rms': 
-        slopes = ((tan(theta) / tan(theta0)**2) * 
-                  exp(-tan(theta)**2 / (2 * tan(theta0)**2)))
-    elif dist == 'rmsnorm':
-        slopes = (1/(2*pi*tan(theta0)**2 * cos(theta)**3) * 
-                  exp(-tan(theta)**2 / (2 * tan(theta0)**2)))
+    if dist == "rms":
+        slopes = (tan(theta) / tan(theta0) ** 2) * exp(
+            -tan(theta) ** 2 / (2 * tan(theta0) ** 2)
+        )
 
     # Calculate theta-bar slope distribution (Hapke 1984)
-    elif dist == 'tbar':
-        slopes = ((2 * sin(theta) / (pi * tan(theta0)**2 * cos(theta)**2)) *
-                 exp((-tan(theta)**2) / (pi * tan(theta0)**2)))
+    elif dist == "tbar":
+        slopes = (
+            2 * sin(theta) / (pi * tan(theta0) ** 2 * cos(theta) ** 2)
+        ) * exp((-tan(theta) ** 2) / (pi * tan(theta0) ** 2))
     else:
-        raise ValueError('Invalid distribution specified (accepts rms, tbar).')
+        raise ValueError("Invalid distribution specified (accepts rms, tbar).")
     return slopes
--- tests/test_roughness.py
+++ tests/test_roughness.py
@@ -1,25 +1,76 @@
 """Test module for roughness."""
-
-from roughness import __author__, __email__, __version__
+import pytest
+import numpy as np
+from roughness import __author__, __email__
+from roughness.roughness import sph2cart, slope_dist, visible_slopes
 
 
 def test_project_info():
     """Test __author__ value."""
     assert __author__ == "Christian Tai Udovicic"
     assert __email__ == "cj.taiudovicic@gmail.com"
-    assert __version__ == "0.0.0"
 
 
-def test_sph2cart():
-    # TODO
-    pass
+def test_sph2cart_1d():
+    """Test sph2cart with numeric arguments."""
+    actual = sph2cart(np.radians(45), np.radians(45), 1)
+    expected = np.array((0.5, 0.5, np.sqrt(0.5)))
+    np.testing.assert_array_almost_equal(actual, expected)
 
 
-def test_slope_dist():
-    # TODO
-    pass
+def test_sph2cart_2d():
+    """Test sph2cart with 2D array arguments."""
+    theta = np.array(([30, 45], [45, 30]))
+    azimuth = np.array(([30, 45], [30, 45]))
+    actual = sph2cart(np.radians(theta), np.radians(azimuth), 1)
+    expected = np.sqrt(
+        np.array(
+            [
+                [[0.1875, 0.0625, 0.75], [0.25, 0.25, 0.5]],
+                [[0.375, 0.125, 0.5], [0.125, 0.125, 0.75]],
+            ]
+        )
+    )
+    np.testing.assert_array_almost_equal(actual, expected)
 
 
-def test_visible_slopes():
-    # TODO
-    pass
+def test_slope_dist_rms():
+    """Test slope_dist for RMS slope distribution."""
+    actual = slope_dist((0, 10, 20), 20, "rms")
+    expected = np.array([0, 0.12421774, 0.27111624])
+    np.testing.assert_array_almost_equal(actual, expected)
+
+
+def test_slope_dist_tbar():
+    """Test slope_dist for theta-bar slope distribution."""
+    actual = slope_dist((0, 10, 20), 20, "tbar")
+    expected = np.array([0, -0.09569568, 0.50721859])
+    np.testing.assert_array_almost_equal(actual, expected)
+
+
+def test_slope_dist_unknown():
+    """Test slope_dist fails for unknown slope distribution."""
+    with pytest.raises(ValueError):
+        slope_dist((0, 10, 20), 20, "???")
+
+
+def test_visible_slopes_shape():
+    """Test visible_slopes output shape."""
+    assert visible_slopes(0, 0, 20).shape == (360, 90)
+    assert visible_slopes(0, 0, 20, ntheta=41, naz=183).shape == (183, 41)
+
+
+def test_visible_slopes_low_emiss():
+    """Test visible_slopes at low emission angle."""
+    actual = visible_slopes(0, 0, 20, ntheta=3, naz=2)
+    expected = np.array(
+        [[0, 0.09663027, 0.12822571], [0, 0.09663027, 0.12822571]]
+    )
+    np.testing.assert_array_almost_equal(actual, expected)
+
+
+def test_visible_slopes_high_emiss():
+    """Test visible_slopes at high emission angle."""
+    actual = visible_slopes(60, 0, 20, ntheta=3, naz=2)
+    expected = np.array([[0, 0.19326055, 0.51290283], [0, 0, 0]])
+    np.testing.assert_array_almost_equal(actual, expected)
