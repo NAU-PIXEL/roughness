Fix scale factors and weighting bugs

roughness/diviner.py
@@ -1,6 +1,8 @@
 """Helpers for Diviner data."""
+from functools import lru_cache
 from pathlib import Path
 import numpy as np
+import pandas as pd
 import xarray as xr
 import rasterio as rio
 import roughness.helpers as rh
@@ -14,6 +16,11 @@ DIV_WL1 = np.array([0.0001, 8.075, 8.40, 13, 25, 50, 100])
 DIV_WL2 = np.array([8.075, 8.40, 13, 25, 50, 100, 500])
 DIV_TMIN = np.array([190, 190, 180, 95, 60, 40, 0])
 ITERS = np.array([5, 5, 6, 10, 16, 24, 56])
+FILT_SCALE = np.array(
+    [1.0005, 1.0003, 1.0010, 1.0018, 1.00600, 0.71939, 0.79358]
+)
+FDIV_T2R = "/home/ctaiudovicic/projects/roughness/tmp/div_t2r.txt"
+FDIV_FILT = "/nfs/data/josh/home/projects/diviner/diviner_filter_functions.hdf"
 
 
 def lev4hourly2xr(
@@ -237,11 +244,73 @@ def div_tbol(divbt, wl1=DIV_WL1, wl2=DIV_WL2, tmin=DIV_TMIN, iters=ITERS):
     for i in range(7)[::-1]:  # Start at C9 and work backwards
         if divbt[i] > tmin[i]:
             temp[i] = divbt[i]
-        else:
+        elif divbt[i] > 0:
             temp[i] = temp[i + 1]  # Use bt from next channel over
+        else:
+            temp[i] = np.nan
         rad[i] = SIGMA * temp[i] ** 4 * cfst(temp[i], wl1[i], wl2[i], iters[i])
     tbol = (np.sum(rad) / SIGMA) ** 0.25
     return tbol
+
+
+def load_div_filters(
+    fdiv_filt=FDIV_FILT, scale=True, wlunits=True, bands=DIV_C
+):
+    """
+    Return Diviner filter functions
+    """
+    hdf = xr.load_dataset(fdiv_filt)
+    wn = hdf.xaxis.values.squeeze()
+    data = hdf.data.squeeze().values
+    data = data.reshape(data.shape[1], data.shape[0])  # reads wrong axis order
+    div_filt = xr.DataArray(
+        data,
+        name="Normalized Response",
+        dims=["wavenumber", "band"],
+        coords={"band": bands, "wavenumber": wn},
+    )
+    if scale:
+        div_filt = div_filt * FILT_SCALE[np.newaxis, :]
+    if wlunits:
+        div_filt.coords["wavelength"] = 10000 / div_filt.wavenumber
+        div_filt = div_filt.swap_dims({"wavenumber": "wavelength"})
+    return div_filt
+
+
+@lru_cache(1)
+def load_div_t2r(fdiv_t2r=FDIV_T2R, tmin=10, tmax=None):
+    """
+    Return Diviner temperature to radiance lookup table.
+    """
+    t2r = pd.read_csv(fdiv_t2r, sep="\s+", index_col=0, header=0)
+    t2r = xr.DataArray(t2r, dims=["temperature", "band"], name="radiance")
+    return t2r.sel(temperature=slice(tmin, tmax), drop=True)
+
+
+def divfilt_rad(wnrad, div_filt):
+    """
+    Return radiance of Diviner bands (convolve rad with Diviner filter funcs).
+    """
+    filtered = div_filt * wnrad  # Must both have wavelength coord
+    return filtered.sum(dim="wavelength") * 2
+
+
+def divrad2bt(divrad, fdiv_t2r=FDIV_T2R):
+    """
+    Return brightness temperatures from Diviner radiance (e.g. from div_filt).
+    """
+    t2r = load_div_t2r(fdiv_t2r)  # cached lookup
+    dbt = np.zeros(len(divrad.band.values))
+    for i, band in enumerate(divrad.band.values):
+        # Interpolate lookup to convert radiance to brightness temperature
+        dbt[i] = np.interp(
+            divrad.sel(band=band),  # desired x-coords (radiance)
+            t2r.sel(band=band),  # actual x-coords (radiance)
+            t2r.temperature,  # actual y-coords (temperature)
+            left=np.nan,
+            right=np.nan,
+        )
+    return dbt
 
 
 def cfst(bt, wl1, wl2, n_iter=100):

roughness/helpers.py
@@ -439,7 +439,7 @@ def inc_to_tloc(inc, az):
     return tloc
 
 
-def tloc_to_inc(tloc, lat=0):
+def tloc_to_inc(tloc, lat=0, az=False):
     """
     Return the solar incidence angle given the local time in hrs.
 
@@ -455,13 +455,39 @@ def tloc_to_inc(tloc, lat=0):
     inc: (float)
         Solar incidence (0, 90) [deg]
     """
-    latr = np.radians(lat)
-    hour_angle = np.radians(15 * (tloc - 12))  # (morning is -; afternoon is +)
-    inc = np.arccos(np.cos(hour_angle) * np.cos(latr))
-    # az = np.arccos(-np.cos(hour_angle) * np.sin(latr) / np.cos(np.pi/2 - inc))
-    # if hour_angle > 0:
-    #     az = 2 * np.pi - az
-    return np.rad2deg(inc)
+    latr = np.deg2rad(lat)
+    hr_angle = np.deg2rad(15 * (tloc - 12))  # (morning is -; afternoon is +)
+    inc = np.rad2deg(np.arccos(np.cos(hr_angle) * np.cos(latr)))
+    if az:
+        az = np.arctan2(np.sin(hr_angle), -np.sin(latr) * np.cos(-hr_angle))
+        az = (np.rad2deg(az) + 360) % 360  # [0, 360) from N
+        return inc, az
+    return inc
+
+
+def get_inc_az_from_subsolar(lon, lat, sslon, sslat):
+    """
+    Return inc and az angle at (lat, lon) given subsolar point (sslat, sslon).
+
+    From calculator at https://the-moon.us/wiki/Sun_Angle
+    """
+    dlon = np.deg2rad(lon - sslon)
+    lon, lat, sslon, sslat = (
+        np.deg2rad(lon),
+        np.deg2rad(lat),
+        np.deg2rad(sslon),
+        np.deg2rad(sslat),
+    )
+    inc = np.arccos(
+        np.sin(sslat) * np.sin(lat)
+        + np.cos(sslat) * np.cos(lat) * np.cos(dlon)
+    )
+    az = np.arctan2(
+        np.cos(sslat) * np.sin(-dlon),
+        np.cos(lat) * np.sin(sslat)
+        - np.sin(lat) * np.cos(sslat) * np.cos(-dlon),
+    )
+    return np.rad2deg(inc), np.rad2deg(az)
 
 
 # Linear algebra

roughness/plotting.py
@@ -1,5 +1,6 @@
 """Helpers for plotting roughness tables and arrays."""
 import numpy as np
+import xarray as xr
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import roughness.m3_correction as m3
@@ -22,6 +23,7 @@ def plot_slope_az_table(
     proj=None,
     vmin=None,
     vmax=None,
+    extent=None,
     **kwargs,
 ):
     """
@@ -49,11 +51,21 @@ def plot_slope_az_table(
         vmin = np.nanmin(table)
     if vmax is None:
         vmax = np.nanmax(table)
+    if extent is None:
+        extent = (0, 90, 360, 0)
+    if isinstance(table, (xr.DataArray, xr.Dataset)):
+        table = table.transpose("az", "theta")
+        extent = (
+            table.theta.min(),
+            table.theta.max(),
+            table.az.min(),
+            table.az.max(),
+        )
 
     if proj == "polar":
         # Define polar coords R ~ slope, Theta ~ azimuth
-        r = np.linspace(0, 90, table.shape[1])
-        theta = np.linspace(0, 360, table.shape[0])
+        r = np.linspace(*extent[:2], table.shape[1])
+        theta = np.linspace(*extent[2:], table.shape[0])
         R, Theta = np.meshgrid(r, theta)
         p = ax.pcolormesh(
             np.deg2rad(Theta),
@@ -74,8 +86,8 @@ def plot_slope_az_table(
     else:
         p = ax.imshow(
             table,
-            extent=(0, 90, 360, 0),
-            aspect=90 / 360,
+            extent=extent,
+            aspect=(extent[1] - extent[0]) / (extent[3] - extent[2]),
             cmap=CMAP_R if cmap_r else CMAP,
             vmin=vmin,
             vmax=vmax,

roughness/roughness.py
@@ -27,18 +27,18 @@ def get_shadow_table(rms, sun_theta, sun_az, los_lookup=FLOOKUP):
     return 1 - get_los_table(rms, sun_theta, sun_az, los_lookup, "prob")
 
 
-def get_view_table(rms, sc_theta, sc_az, weighted=True, los_lookup=FLOOKUP):
+def get_view_table(rms, sc_theta, sc_az, los_lookup=FLOOKUP):
     """
-    Return probability that each facet is observed from (sc_theta, sc_az)
-    on a surface with rms roughness. If weighted is True, the probability is
-    weighted by the cosine of the angle between the facet and view direction.
+    Return probability each facet is observed from (sc_theta, sc_az).
+
+    Weights each facet bin by cos(angle) between facet and view vector. Facets
+    oriented at > 90 deg to view vector are set to NaN.
 
     Parameters
     ----------
     rms_slope (num): Root-mean-square slope of roughness [deg]
     sc_theta (num or array): Spacecraft emission angle [deg]
     sc_az (num or array): Spacecraft azimuth [deg]
-    weighted (bool): Whether to weight by cos(angle) between facet and view
     los_lookup (path or 4D array): Path to file or los lookup array
 
     Returns
@@ -46,12 +46,58 @@ def get_view_table(rms, sc_theta, sc_az, weighted=True, los_lookup=FLOOKUP):
     view_prob (2D array): View probability table (dims: az, theta)
     """
     view = get_los_table(rms, sc_theta, sc_az, los_lookup, "prob")
-    if weighted:
-        view *= get_view_weights(view.theta, view.az, sc_theta, sc_az)
-    return view
+    view *= get_view_cos_theta(view.theta, view.az, sc_theta, sc_az)
+    return view.where(view > 0)  # drop negative (angled away from sc)
 
 
-def get_view_weights(facet_theta, facet_az, sc_theta, sc_az):
+def get_facet_table(rms, inc, los_lookup=FLOOKUP):
+    """
+    Return probability of each facet occurring given an rms rough at solar inc.
+
+    Solar inc is required because very oblique angles may lower the number of
+    valid facets used to compute the los_lookup from DEM (see make_los_table).
+
+    Parameters
+    ----------
+    rms (num): Root-mean-square surface theta describing roughness [deg]
+    inc (num or array): Inclination angle [deg]
+    los_lookup (path or xarray): Los lookup xarray or path to file.
+
+    Returns
+    -------
+    prob (2D array): Binned probability of facet occurring (dims: az, theta).
+    """
+    total = get_los_table(rms, inc, None, los_lookup, "total")
+    # If rms=0 (or < first theta), assume surface is flat
+    if rms < total.theta.values[1]:
+        prob = xr.zeros_like(total)
+        prob.loc[{"theta": 0}] = 1  # flat => prob=1 for all theta=0
+    else:
+        prob = total / total.sum()  # .sum skips NaN
+    return prob
+
+
+def get_weight_table(rms, sun_theta, sc_theta, sc_az, flookup=None):
+    """
+    Return probability of each facet existing and being visible from sc.
+
+    Parameters
+    ----------
+    rms (arr): RMS roughness [degrees]
+    sun_theta (arr): Sun zenith angle [degrees]
+    sc_theta (arr): Sc zenith angle [degrees]
+    sc_az (arr): Sc azimuth angle [degrees]
+    flookup (path or xarray): Facet lookup xarray or path to file.
+    """
+    # Get probability each facet is in view, weighted by cos(i) to sc vector
+    view_table = get_view_table(rms, sc_theta, sc_az, flookup)
+    # Get probability each facet exists at given rms
+    facet_weight = get_facet_table(rms, sun_theta, flookup)
+    # Final probability is product of view and facet probabilities
+    return view_table * facet_weight
+
+
+def get_view_cos_theta(facet_theta, facet_az, sc_theta, sc_az):
     """
     Return weighting factor for surface facets (facet_theta, facet_az) as
     viewed from (sc_theta, sc_az) as cos(angle) between facet and view vectors.
@@ -69,28 +115,6 @@ def get_view_weights(facet_theta, facet_az, sc_theta, sc_az):
     facet_cart = rh.sph2cart(*np.meshgrid(ft, fa))
     sc_cart = rh.sph2cart(st, sa)
     return rh.element_dot(facet_cart, sc_cart)
-
-
-def get_facet_weights(rms, inc, los_lookup=FLOOKUP):
-    """
-    Return the weighted probability of each facet occurring given an rms rough
-    surface facets in the line of sight of (theta, az).
-
-    Parameters
-    ----------
-    rms (num): Root-mean-square surface theta describing roughness [deg]
-    inc (num or array): Inclination angle [deg]
-    los_lookup (path or xarray): Los lookup xarray or path to file.
-
-    Returns
-    -------
-    weights (2D array): Weighted probability table that sums to 1.
-    """
-    tot = get_table_xarr([rms, inc], ["rms", "inc"], None, "total", los_lookup)
-    if rms <= tot.theta.values[1]:
-        tot = xr.zeros_like(tot)
-        tot.loc[{"theta": 0}] = 1
-    return tot / tot.sum(skipna=True)
 
 
 def get_los_table(rms, inc, az=None, los_lookup=FLOOKUP, da="prob"):